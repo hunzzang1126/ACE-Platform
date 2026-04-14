b4f5db63b202c88f3298b37c4b7ace7a6cf1d010
Young Hoon An <hunzzang1126@gmail.com>
2026-04-14 09:21:33 -0400

    chore: bump to v0.0.0.528

--- src/version.ts
+++ src/version.ts
@@ -4,4 +4,4 @@
 // ★ RULE: Increment BUILD number before every `git push`.
 // Format: v{major}.{minor}.{patch}.{build}
 
-export const APP_VERSION = 'v0.0.0.527';
+export const APP_VERSION = 'v0.0.0.528';
